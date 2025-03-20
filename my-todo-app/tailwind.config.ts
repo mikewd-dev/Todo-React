515a80394a0f0b278082d1051586238e59d85f5d
Michael Dawson <37080386-mwilldawson@users.noreply.replit.com>
2025-03-20 00:08:06 +0000

    initial commit

---
+++ my-todo-app/tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import { defineConfig } from "tailwindcss";
+
+export default defineConfig({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
--- my-todo-app/vite.config.ts
+++ my-todo-app/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
-})
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: [
+      "a0a1397c-219d-48a5-9b71-4b45d9ae102f-00-3kd4t1wodgi0k.picard.replit.dev",
+    ],
+  },
+});
