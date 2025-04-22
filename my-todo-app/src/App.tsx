f945e55969070735c51c133afcfb21f00ebe0c6a
mikewd-dev <145623651+mikewd-dev@users.noreply.github.com>
2025-04-22 17:26:37 +0000

    dark-mode feature added

--- my-todo-app/src/App.tsx
+++ my-todo-app/src/App.tsx
@@ -1,19 +1,23 @@
-;import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
 
 import './App.css'
 import NewTodo from './NewTodo'
 
-function App() {
+const App = () =>{
+  const [darkMode, setDarkMode] = useState(false)
+  useEffect(() => {document.body.className = darkMode ? 'darkMode' : ''}, [darkMode])
   return (
     <div className="todoTitle">
       <div className="title">
         <h1>TODO</h1>
-        <FontAwesomeIcon icon={faSun} style={{ marginLeft: '10px' }} />
+        <button onClick={() => setDarkMode(prev => !prev)}><FontAwesomeIcon icon={darkMode? faSun:faMoon} style={{ marginLeft: '10px' }} /></button>
       </div>
       <NewTodo />
     </div>
+  
   )
 }
 
