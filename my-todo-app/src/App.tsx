966c964d2fd1209a13e6d3e7fdcdead35f471148
mikewd-dev <145623651+mikewd-dev@users.noreply.github.com>
2025-04-22 10:12:04 +0000

    added fontawesome icon

--- my-todo-app/src/App.tsx
+++ my-todo-app/src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+;import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+
 import './App.css'
 import NewTodo from './NewTodo'
+
 function App() {
-  return(
-    <div>
-      <NewTodo/>
+  return (
+    <div className="todoTitle">
+      <div className="title">
+        <h1>TODO</h1>
+        <FontAwesomeIcon icon={faSun} style={{ marginLeft: '10px' }} />
+      </div>
+      <NewTodo />
     </div>
   )
-
 }
 
-export default App
+export default App;
