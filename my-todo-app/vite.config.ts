d6249ae47ef362f7b2cb953bbf2b3d1c7ebdd818
mikewd-dev <145623651+mikewd-dev@users.noreply.github.com>
2025-05-21 00:49:20 +0000

    fixes vite config

--- my-todo-app/vite.config.ts
+++ my-todo-app/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: "0.0.0.0",
     port: 5173,
