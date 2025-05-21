3c1fb1988cea214ee47e00255bbdfdbcef250d39
mikewd-dev <145623651+mikewd-dev@users.noreply.github.com>
2025-05-21 12:45:37 +0000

    updates config

---
+++ my-todo-app/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        allowedHosts: [
+            "7a6f73ee-d54d-47b6-aabf-fb8bff0b7ab0-00-3rqjno6ohyscf.riker.replit.dev",
+        ],
+    },
+});
