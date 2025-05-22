ade4fc9e8eb070b32f6ec79357c0bdcf70e46f7b
mikewd-dev <145623651+mikewd-dev@users.noreply.github.com>
2025-05-22 13:39:23 +0000

    makes change to App.tsx

--- my-todo-app/src/App.tsx
+++ my-todo-app/src/App.tsx
@@ -23,12 +23,11 @@
         return (
           <div className="min-h-screen relative w-full text-white font-custom-font overflow-hidden">
             <div className="absolute inset-0 z-0 2xl:bg-back-light dark:bg-back-dark" />
-            <div className="absolute inset-0 z-0 h-[185px] top-0 left-0 2xl:bg-[url('/assets/bg-desktop-light.jpg')] dark:bg-[url('/assets/bg-desktop-dark.jpg')] bg-center-center bg-contain bg-cover bg-no-repeat bg-fixed bg-top opacity-90" />
+            <div className="absolute inset-0 z-0 h-[185px] top-0 left-0 bg-[url('/assets/bg-desktop-light.jpg')] dark:bg-[url('/assets/bg-desktop-dark.jpg')] bg-center-center bg-contain bg-cover bg-no-repeat bg-fixed bg-top opacity-90" />
             <div className="relative z-10 flex flex-col items-center justify-start text-[18px]">
             
               <div className="flex justify-between items-center mt-[30px] w-[90vw] h-[20px]">
                 <h1 className="font-bold-size tracking-widest">TODO</h1>
-                <p>This is to test why there are no changes to netlify</p>
                 <button onClick={toggleDarkMode}>
                   <FontAwesomeIcon
                     icon={darkMode ? faSun : faMoon}
