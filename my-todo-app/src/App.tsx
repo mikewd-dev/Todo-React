f4ed345eb8a5e769a2e2fe50090df7da8d23835d
mikewd-dev <145623651+mikewd-dev@users.noreply.github.com>
2025-05-22 10:46:49 +0000

    applies fixes

--- my-todo-app/src/App.tsx
+++ my-todo-app/src/App.tsx
@@ -21,9 +21,9 @@
         const toggleDarkMode = () => setDarkMode((prev) => !prev);
 
         return (
-          <div className="min-h-screen   relative w-full text-white font-custom-font overflow-hidden">
-            <div className="absolute inset-0 z-0 bg-back-light dark:bg-back-dark" />
-            <div className="absolute inset-0 z-0 h-[185px] top-0 left-0 bg-[][url('/assets/bg-desktop-light.jpg')] dark:bg-[url('/assets/bg-desktop-dark.jpg')] bg-center-center bg-contain bg-cover bg-no-repeat bg-fixed bg-top opacity-90" />
+          <div className="min-h-screen relative w-full text-white font-custom-font overflow-hidden">
+            <div className="absolute inset-0 z-0 2xl:bg-back-light dark:bg-back-dark" />
+            <div className="absolute inset-0 z-0 h-[185px] top-0 left-0 2xl:bg-[][url('/assets/bg-desktop-light.jpg')] dark:bg-[url('/assets/bg-desktop-dark.jpg')] bg-center-center bg-contain bg-cover bg-no-repeat bg-fixed bg-top opacity-90" />
             <div className="relative z-10 flex flex-col items-center justify-start text-[18px]">
             
               <div className="flex justify-between items-center mt-[30px] w-[90vw] h-[20px]">
